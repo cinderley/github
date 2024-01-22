266b08f8a3b4c0a48ab78735b446b889b7f10865
Ashley <ashley.clark1@myokanagan.bc.ca>
2024-01-22 13:24:16 -0800

    creating controller endpoints

--- github/src/main/java/com/cosc224/github/entity/User.java
+++ github/src/main/java/com/cosc224/github/entity/User.java
@@ -1,4 +1,11 @@
 package com.cosc224.github.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
+    String name;
+    String email;
 }
